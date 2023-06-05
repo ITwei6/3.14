518e3cc00eb7a117ccceba023844e88d60a8c44e
2268129437@qq.com <2268129437@qq.com>
2023-06-05 18:08:25 +0800

    栈和队列

---
+++ 3.13/3.13/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+
+	int a = 0, y = 1;
+	if (a == 0)
+		if (y == 0)
+			printf("正确\n");
+	else
+		{
+			printf("错误\n");
+		}
+	case '\n':
+		linecount++;
+		//该处省略break语句
+	case '\t':
+	case ' ':
+	return 0;
+}
